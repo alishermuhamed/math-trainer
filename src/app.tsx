e8b770b0e17775e456d1ef7d15bb075081fd881b
alishermuhamed <alisher.muhamed@yandex.ru>
2019-04-11 18:07:42 +0300

    Updated app as a functional component

--- src/app.tsx
+++ src/app.tsx
@@ -3,15 +3,11 @@ import { Switch, Route } from 'react-router-dom';
 import MainPage from './pages/main-page';
 import GamePage from './pages/game-page';
 
-class App extends React.Component {
-  render(): JSX.Element {
-    return (
-      <Switch>
-        <Route exact={true} path="/" component={MainPage} />
-        <Route path="/game" component={GamePage} />
-      </Switch>
-    );
-  }
-}
+const App: React.FC = (): JSX.Element => (
+  <Switch>
+    <Route exact={true} path="/" component={MainPage} />
+    <Route path="/game" component={GamePage} />
+  </Switch>
+);
 
 export default App;
