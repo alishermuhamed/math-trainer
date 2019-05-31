f31b95c324c91d5481f1db1a7d0042171ed58f5d
Alisher Mukhametzhanov <a.mukhametzhanov@MacBook-Pro-Alisher.local>
2019-05-31 07:28:44 +0300

    Add settings page

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,13 @@ import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from './pages/main-page';
 import GamePage from './pages/game-page';
+import SettingsPage from './pages/settings-page';
 
 const App: React.FC = (): JSX.Element => (
   <Switch>
     <Route exact={true} path="/" component={MainPage} />
     <Route path="/game" component={GamePage} />
+    <Route path="/settings" component={SettingsPage} />
   </Switch>
 );
 
---
+++ src/pages/settings-page.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+
+const SettingsPage: React.FC = (): JSX.Element => <Link to="/">Go Back</Link>;
+
+export default SettingsPage;
