4d2b056e19e04e8d29a447174e40f5615cf961b8
Alisher Mukhametzhanov <a.mukhametzhanov@MacBook-Pro-Alisher.local>
2019-05-31 13:31:17 +0300

    Link component receives class names as props

--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -5,10 +5,11 @@ import './style.css';
 interface Props {
   to: string;
   children: string;
+  className?: string;
 }
 
 const Link: React.FC<Props> = (props: Props): JSX.Element => (
-  <RouterLink to={props.to} className="link">
+  <RouterLink to={props.to} className={'link ' + props.className}>
     {props.children}
   </RouterLink>
 );
