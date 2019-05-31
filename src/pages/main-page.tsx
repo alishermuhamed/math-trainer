9725558840084af6c105fc1ab694646bdeaaf8fe
Alisher Mukhametzhanov <a.mukhametzhanov@MacBook-Pro-Alisher.local>
2019-05-31 07:29:25 +0300

    Add links to main page

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-const MainPage: React.FC = (): JSX.Element => <Link to="/game">Go To Game</Link>;
+const MainPage: React.FC = (): JSX.Element => (
+  <React.Fragment>
+    <Link to="/game" className="link large-text">
+      START GAME
+    </Link>
+    <Link to="/settings" className="link large-text">
+      SETTINGS
+    </Link>
+  </React.Fragment>
+);
 
 export default MainPage;
