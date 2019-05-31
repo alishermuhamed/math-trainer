063c34c0c9f09ce4bc92472ec199f707656809bb
Alisher Mukhametzhanov <a.mukhametzhanov@MacBook-Pro-Alisher.local>
2019-05-31 07:30:08 +0300

    Add adding exercise

--- src/pages/game-page.tsx
+++ src/pages/game-page.tsx
@@ -1,6 +1,63 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-const GamePage: React.FC = (): JSX.Element => <Link to="/">Go Back</Link>;
+interface State {
+  firstNumber: number;
+  operation: string;
+  secondNumber: number;
+  answer: number;
+  userInput: string;
+}
+
+class GamePage extends React.Component<{}, State> {
+  state = {
+    firstNumber: 0,
+    operation: '+',
+    secondNumber: 0,
+    answer: 0,
+    userInput: '',
+  };
+
+  componentDidMount = (): void => this.generateNewExercise();
+
+  generateNewExercise = (): void => {
+    const firstNumber = Math.floor(Math.random() * 100);
+    const secondNumber = Math.floor(Math.random() * 100);
+    const answer = eval(`${firstNumber} ${this.state.operation} ${secondNumber}`);
+    this.setState({
+      firstNumber,
+      secondNumber,
+      answer,
+    });
+  };
+
+  handleUserInputChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    const { value } = event.currentTarget;
+    if (parseInt(value) === this.state.answer) {
+      this.generateNewExercise();
+      this.setState({ userInput: '' });
+    } else this.setState({ userInput: value });
+  };
+
+  render(): JSX.Element {
+    const { firstNumber, operation, secondNumber, userInput } = this.state;
+    return (
+      <React.Fragment>
+        <Link to="/" className="link">
+          STOP
+        </Link>
+        <span className="large-text">{`${firstNumber} ${operation} ${secondNumber}`}</span>
+        <input
+          type="number"
+          pattern="\d*"
+          className="user-input"
+          autoFocus={true}
+          value={userInput}
+          onChange={this.handleUserInputChange}
+        />
+      </React.Fragment>
+    );
+  }
+}
 
 export default GamePage;
